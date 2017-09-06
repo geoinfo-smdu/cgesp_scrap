f910f9c30d2a6401ec22f42f8c9ed53d5a000610
yubathom <yubathom@gmail.com>
2017-09-06 14:01:14 -0300

    limpeza

--- filter_0.py
+++
@@ -1,8 +0,0 @@
-txt = open("com_falhas.js","r")
-content = txt.read() 
-
-new_file = open('novo.js','w')
-new_file.write(content)
-
-txt.close()
-new_file.close()
--- geocoding_tests.py
+++
@@ -1,4 +0,0 @@
-import geocoder
-# g = geocoder.google("avenida ipiranga and avenida são joão, Centro,São Paulo - SP, Brazil")
-# print(g.latlng)
-#[-23.542358, -46.640606]
